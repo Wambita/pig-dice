3886d1a4e4c9de0d979d2aa5a0d93a28dca33c86
Wambita Fana <wambitafana@gmail.com>
2019-03-03 07:12:58 +0300

    Remove about.html file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
- function openNav() {
-     document.getElementById("mySidenav").style.width = "250px";
-          }
-          
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-          }
+// business logic 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+$("#game").submit(function(event) {
+    event.preventDefault();
+    var player1 = $("#player-one").val();
+  var player2 = $("#player-two").val();
+  $("#p-1").text(player1.toUpperCase());
+  $("#p-2").text(player2.toUpperCase());
+  
+      if ($("#player-one").val() === "" || $("#player-two").val() === "") {
+        alert("Please fill in your names to continue!");
+      } else {
+        $("#show").hide();
+        $("#show").show();
+      }
+  });
+});
+$(document).ready(function() {
+    $("#mySidenav").click(function() {
+    $("#mySidenav").hide();
+    $("#rules").slideDown(500);
+    $("#rules").show();
+});
+});
